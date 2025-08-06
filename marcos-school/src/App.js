1421e4f62eea40c01805daf90b4f2f7ed2e800d6
Marcos Gabriel <marcosindev@gmail.com>
2025-08-06 19:33:21 -0300

    adjustment login area & estilized

--- marcos-school/src/App.js
+++ marcos-school/src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
+import Logo from './img/LOGO.webp'
 
 function App() {
   return (
-    <div>
-      <form>
-        <label>
+    <div className="container">
+      <form className="content">
+        <label className="info-area">
           Área de Login
         </label>
+        <img src={Logo} />
         <input type="email" placeholder="Insira seu email" required />
         <input type="password" placeholder="Insira sua senha" required />
-        <button type="submit">
+        <button type="submit" className="login-button">
           Entrar
         </button>
+        <span className="warning-login">
+          Apenas usuários credenciados podem ter acesso ao sistema.
+        </span>
       </form>
     </div>
   );
