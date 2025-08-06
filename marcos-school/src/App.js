b611614d85a113ad2d4d227c4ef7181796e4b8ab
Marcos Gabriel <marcosindev@gmail.com>
2025-08-06 19:39:17 -0300

    Update App.js

--- marcos-school/src/App.js
+++ marcos-school/src/App.js
@@ -5,18 +5,25 @@ function App() {
   return (
     <div className="container">
       <form className="content">
+
         <label className="info-area">
           Área de Login
         </label>
+
         <img src={Logo} />
+
         <input type="email" placeholder="Insira seu email" required />
+
         <input type="password" placeholder="Insira sua senha" required />
+
         <button type="submit" className="login-button">
           Entrar
         </button>
+
         <span className="warning-login">
           Apenas usuários credenciados podem ter acesso ao sistema.
         </span>
+
       </form>
     </div>
   );
