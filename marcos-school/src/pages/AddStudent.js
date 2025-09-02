1b24980c8b7e6b2153b8ebd7f875ba79a317a71b
Marcos Gabriel <marcosindev@gmail.com>
2025-09-02 17:35:42 -0300

    Update AddStudent.js

--- marcos-school/src/pages/AddStudent.js
+++ marcos-school/src/pages/AddStudent.js
@@ -21,7 +21,7 @@ function AddStudent() {
     const searchStudents = async (name_student = '') => {
         setStudentsData([])
         try {
-            const response = await axios.post('http://  localhost:5000/api/fetchstudents', {
+            const response = await axios.post('http://localhost:5000/api/fetchstudents', {
                 name_student: searchStudentApi
             })
             setStudentsData(response.data)
